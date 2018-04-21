5aea1b492354fef3dbd130b8ccc3f6095d800782
Ai Nguyen <ainguyenkaka@gmail.com>
2018-04-21 11:13:19 +0800

    init

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <h2 class="center-align blue-text animated fadeInDown">
+      <strong>
+        Hello
+      </strong>
+    </h2>
+    <h4  class="center-align blue-text animated fadeInUp subtitle-animated">
+      <strong>
+        Have a nice day !
+      </strong>
+    </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .subtitle-animated {
+    animation-duration: 1s;
+    animation-delay: 1s;
+  }
+</style>
